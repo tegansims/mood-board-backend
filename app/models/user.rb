1ed860f5e0f571d348d27adaf68e2d3dd1c6a8b5
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-22 20:50:58 +0100

    accepts_nested_attributes added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
     has_many :entries
     has_many :categories, through: :entries
     has_secure_password
+    accepts_nested_attributes_for :categories, :entries
+
 
     validates :email, presence: true
     validates :password, presence: true
